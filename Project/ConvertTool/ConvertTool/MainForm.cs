bf6916d24a631faf4889ba418fb9cfc2c73044f9
xiaodong tang <1297953037@qq.com>
2019-10-08 14:05:04 +0800

    add document convert tool

---
+++ Project/ConvertTool/ConvertTool/MainForm.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Spire.Pdf;
+using CommonUtils.FileHelper;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Word;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Interop;
+using Microsoft.Office;
+using Microsoft.Office.Core;
+
+namespace ConvertTool
+{
+    public partial class MainForm : Telerik.WinControls.UI.RadForm
+    {
+        private string sourchFilePath;
+        private string currentSaveFilePath;
+        private DocumentType documentType;
+        public MainForm()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        enum DocumentType
+        {
+            PdfToWord,
+            PdfToExcel,
+            PdfToPPT,
+            WordToPDF,
+            ExcelToPDF,
+            PptToPDF
+        }
+
+        private void PDFToWord(string sourchFilePath)
+        {
+            if (sourchFilePath.Contains(".pdf"))
+            {
+                currentSaveFilePath = sourchFilePath.Replace(".pdf", "") + ".doc";
+            }
+            PdfDocument doc = new PdfDocument();
+            doc.LoadFromFile(sourchFilePath);
+            doc.SaveToFile(currentSaveFilePath, FileFormat.DOCX);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.menu_addFile.Click += Menu_addFile_Click;
+            this.btn_startConvert.Click += Btn_startConvert_Click;
+            this.radDock1.ActiveWindowChanged += RadDock1_ActiveWindowChanged;
+        }
+
+        private void RadDock1_ActiveWindowChanged(object sender, Telerik.WinControls.UI.Docking.DockWindowEventArgs e)
+        {
+            if (this.radDock1.ActiveWindow == this.dtwordtopdf)
+                this.documentType = DocumentType.WordToPDF;
+            else if (this.radDock1.ActiveWindow == this.dtexceltopdf)
+                this.documentType = DocumentType.ExcelToPDF;
+            else if (this.radDock1.ActiveWindow == this.dtppttopdf)
+                this.documentType = DocumentType.PptToPDF;
+            else if (this.radDock1.ActiveWindow == this.dt_pdftoexcel)
+                this.documentType = DocumentType.PdfToExcel;
+            else if (this.radDock1.ActiveWindow == this.dt_pdftoppt)
+                this.documentType = DocumentType.PdfToPPT;
+            else if (this.radDock1.ActiveWindow == this.dt_pdftoword)
+                this.documentType = DocumentType.PdfToWord;
+        }
+
+        private void Btn_startConvert_Click(object sender, EventArgs e)
+        {
+            if (this.documentType == DocumentType.PdfToWord)
+                PDFToWord(sourchFilePath);
+            else if (this.documentType == DocumentType.WordToPDF)
+            {
+                if (sourchFilePath.Contains(".doc"))
+                {
+                    currentSaveFilePath = sourchFilePath.Replace(".doc", "") + ".pdf";
+                }
+                else if (sourchFilePath.Contains(".docx"))
+                {
+                    currentSaveFilePath = sourchFilePath.Replace(".docx", "") + ".pdf";
+                }
+                WordConvertPDF(sourchFilePath,currentSaveFilePath,WdExportFormat.wdExportFormatPDF);
+            }
+
+            //是否启动
+            if (MessageBox.Show("是否立即打开转换后的文件", "提示", MessageBoxButtons.OKCancel, MessageBoxIcon.Information) != DialogResult.OK)
+                return;
+            System.Diagnostics.Process.Start(currentSaveFilePath);
+        }
+
+        private void Menu_addFile_Click(object sender, EventArgs e)
+        {
+            FileContent fileContent = FileSelect.GetSelectFileContent("(*.*)|*.*","选择文件");
+            sourchFilePath = fileContent.FileName;
+        }
+
+        #region office文件转换为pdf文件
+
+        /// <summary>
+        /// 将word文档转换成PDF格式
+        /// </summary>
+        /// <param name="sourcePath">源文件路径</param>
+        /// <param name="targetPath">目标文件路径</param> 
+        /// <param name="exportFormat"></param>
+        /// <returns></returns>
+        private bool WordConvertPDF(string sourcePath, string targetPath, Microsoft.Office.Interop.Word.WdExportFormat exportFormat)
+        {
+            bool result;
+            object paramMissing = Type.Missing;
+            Microsoft.Office.Interop.Word.ApplicationClass wordApplication = new Microsoft.Office.Interop.Word.ApplicationClass();
+            Microsoft.Office.Interop.Word.Document wordDocument = null;
+            try
+            {
+                object paramSourceDocPath = sourcePath;
+                string paramExportFilePath = targetPath;
+
+                Microsoft.Office.Interop.Word.WdExportFormat paramExportFormat = exportFormat;
+                bool paramOpenAfterExport = false;
+                Microsoft.Office.Interop.Word.WdExportOptimizeFor paramExportOptimizeFor =
+                        Microsoft.Office.Interop.Word.WdExportOptimizeFor.wdExportOptimizeForPrint;
+                Microsoft.Office.Interop.Word.WdExportRange paramExportRange = Microsoft.Office.Interop.Word.WdExportRange.wdExportAllDocument;
+                int paramStartPage = 0;
+                int paramEndPage = 0;
+                Microsoft.Office.Interop.Word.WdExportItem paramExportItem = Microsoft.Office.Interop.Word.WdExportItem.wdExportDocumentContent;
+                bool paramIncludeDocProps = true;
+                bool paramKeepIRM = true;
+                Microsoft.Office.Interop.Word.WdExportCreateBookmarks paramCreateBookmarks =
+                        Microsoft.Office.Interop.Word.WdExportCreateBookmarks.wdExportCreateWordBookmarks;
+                bool paramDocStructureTags = true;
+                bool paramBitmapMissingFonts = true;
+                bool paramUseISO19005_1 = false;
+
+                wordDocument = wordApplication.Documents.Open(
+                        ref paramSourceDocPath, ref paramMissing, ref paramMissing,
+                        ref paramMissing, ref paramMissing, ref paramMissing,
+                        ref paramMissing, ref paramMissing, ref paramMissing,
+                        ref paramMissing, ref paramMissing, ref paramMissing,
+                        ref paramMissing, ref paramMissing, ref paramMissing,
+                        ref paramMissing);
+
+                if (wordDocument != null)
+                    wordDocument.ExportAsFixedFormat(paramExportFilePath,
+                            paramExportFormat, paramOpenAfterExport,
+                            paramExportOptimizeFor, paramExportRange, paramStartPage,
+                            paramEndPage, paramExportItem, paramIncludeDocProps,
+                            paramKeepIRM, paramCreateBookmarks, paramDocStructureTags,
+                            paramBitmapMissingFonts, paramUseISO19005_1,
+                            ref paramMissing);
+                result = true;
+            }
+            finally
+            {
+                if (wordDocument != null)
+                {
+                    wordDocument.Close(ref paramMissing, ref paramMissing, ref paramMissing);
+                    wordDocument = null;
+                }
+                if (wordApplication != null)
+                {
+                    wordApplication.Quit(ref paramMissing, ref paramMissing, ref paramMissing);
+                    wordApplication = null;
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 将excel文档转换成PDF格式
+        /// </summary>
+        /// <param name="sourcePath">源文件路径</param>
+        /// <param name="targetPath">目标文件路径</param> 
+        /// <param name="targetType"></param>
+        /// <returns></returns>
+        private bool ExcelConvertPDF(string sourcePath, string targetPath, XlFixedFormatType targetType)
+        {
+            bool result;
+            object missing = Type.Missing;
+            Microsoft.Office.Interop.Excel.ApplicationClass application = null;
+            Workbook workBook = null;
+            try
+            {
+                application = new Microsoft.Office.Interop.Excel.ApplicationClass();
+                object target = targetPath;
+                object type = targetType;
+                workBook = application.Workbooks.Open(sourcePath, missing, missing, missing, missing, missing,
+                        missing, missing, missing, missing, missing, missing, missing, missing, missing);
+
+                workBook.ExportAsFixedFormat(targetType, target, XlFixedFormatQuality.xlQualityStandard, true, false, missing, missing, missing, missing);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (workBook != null)
+                {
+                    workBook.Close(true, missing, missing);
+                    workBook = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// 将ppt文档转换成pdf格式
+        /// </summary>
+        /// <param name="sourcePath">源文件路径</param>
+        /// <param name="targetPath">目标文件路径</param> 
+        /// <param name="targetFileType"></param>
+        /// <returns></returns>
+        private bool PPTConvertPDF(string sourcePath, string targetPath, PpSaveAsFileType targetFileType)
+        {
+            bool result;
+            object missing = Type.Missing;
+            Microsoft.Office.Interop.PowerPoint.ApplicationClass application = null;
+            Presentation persentation = null;
+            try
+            {
+                application = new Microsoft.Office.Interop.PowerPoint.ApplicationClass();
+                //persentation = application.Presentations.Open(sourcePath, MsoTriState.msoTrue, MsoTriState.msoFalse, MsoTriState.msoFalse);
+                //persentation.SaveAs(targetPath, targetFileType, Microsoft.Office.Core.MsoTriState.msoTrue);
+
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+            finally
+            {
+                if (persentation != null)
+                {
+                    persentation.Close();
+                    persentation = null;
+                }
+                if (application != null)
+                {
+                    application.Quit();
+                    application = null;
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            return result;
+        }
+
+        #endregion
+    }
+}
